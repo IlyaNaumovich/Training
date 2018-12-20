f2cc7b89310cdf8d177d656edfc68b26b978141d
Artsem Melniaku <test@test.by>
2018-12-20 10:52:54 +0300

    grid

--- AutomationTestingJava/src/test/java/MarathonTests.java
+++ AutomationTestingJava/src/test/java/MarathonTests.java
@@ -1,10 +1,8 @@
 import helper.Dictionary;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -21,7 +19,7 @@ import java.util.Set;
 
 @Test
 public class MarathonTests {
-    private WebDriver driver;
+    private ThreadLocal<WebDriver> drivers = new ThreadLocal<WebDriver>();
     private String lan;
 
     @BeforeClass
@@ -35,11 +33,23 @@ public class MarathonTests {
     }
 
     @BeforeMethod(alwaysRun = true)
-    @Parameters({"Language"})
-    public void BeforeMethod(@Optional("en") String lang) throws MalformedURLException {
+    @Parameters({"Language", "browserName"})
+    public void BeforeMethod(@Optional("en") String lang,@Optional("chrome") String browserName) throws MalformedURLException {
         //driver = new ChromeDriver();
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/grid/console"), new ChromeOptions());
-        driver.manage().window().maximize();
+        DesiredCapabilities cap = new DesiredCapabilities();
+        if(browserName.equals("chrome")){
+            cap.setCapability("browserName", "chrome");
+            cap.setPlatform(Platform.ANY);
+
+            //driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+            drivers.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap));
+        }else if(browserName.equals("firefox")){
+            //driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), new FirefoxOptions());
+            drivers.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), new FirefoxOptions()));
+        }
+
+
+        drivers.get().manage().window().maximize();
 
         if (lang == null) {
             lang = "en";
@@ -47,7 +57,7 @@ public class MarathonTests {
 
         String url = "https://www.marathonbet.com/%lan%/".replace("%lan%", lang);
 
-        driver.get(url);
+        drivers.get().get(url);
 
         lan = lang;
     }
@@ -64,11 +74,13 @@ public class MarathonTests {
 
     @AfterMethod(alwaysRun = true)
     public void finish() {
-        driver.quit();
+        drivers.get().quit();
+        drivers.set(null);
     }
 
     @Test(groups = {"search"}, dependsOnMethods = {"negativeLogin"})
     public void helloWorldTest() {
+        WebDriver driver = drivers.get();
         driver.findElement(By.className("field-search")).sendKeys("Hello world!" + Keys.ENTER);
 
         Assert.assertTrue(driver.findElements(By.className("search-page")).size() > 0, "Search page should be opened");
@@ -80,6 +92,7 @@ public class MarathonTests {
 
     @Test(groups = {"login", "regression"}, priority = 20)
     public void negativeLogin() {
+        WebDriver driver = drivers.get();
         System.out.println("negativeLogin");
         driver.findElement(By.id("auth_login")).sendKeys("Hello");
         driver.findElement(By.id("auth_login")).sendKeys(Keys.TAB);
@@ -95,6 +108,7 @@ public class MarathonTests {
 
     @Test(groups = {"login", "regression"}, priority = 20, dataProvider = "users")
     public void negativeLogin_dataProvider(String login, String password) {
+        WebDriver driver = drivers.get();
         System.out.println("negativeLogin");
         driver.findElement(By.id("auth_login")).sendKeys(login);
         driver.findElement(By.id("auth_login")).sendKeys(Keys.TAB);
@@ -111,6 +125,7 @@ public class MarathonTests {
     @Test(description = "Colour changed",
             groups = {"css", "regression"}, priority = 50)
     public void colorOfLanguageSwitcherChanged() {
+        WebDriver driver = drivers.get();
         System.out.println("colorOfLanguageSwitcherChanged");
         Actions action = new Actions(driver);
         WebElement languageSwitcher = driver.findElement(By.id("languageSelectField"));
@@ -128,7 +143,7 @@ public class MarathonTests {
     @Test(groups = {"css"})
     public void colorOfLanguageSwitcherChangedSoftAssert() {
         SoftAssert softAssert = new SoftAssert();
-
+        WebDriver driver = drivers.get();
         Actions action = new Actions(driver);
         WebElement languageSwitcher = driver.findElement(By.id("languageSelectField"));
         WebElement languageLabel = languageSwitcher.findElement(By.className("menu-link"));
@@ -147,7 +162,7 @@ public class MarathonTests {
     @Test
     public void verifyLinks() throws IOException {
         Set<String> linkSet = new HashSet<String>();
-
+        WebDriver driver = drivers.get();
         for (WebElement element : driver.findElements(By.xpath("//*[@href]"))) {
             String ref = element.getAttribute("href");
 
